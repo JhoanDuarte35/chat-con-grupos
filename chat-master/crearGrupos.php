99129aacca2394ef4269ed765d579133d1f134de
JhoanDuarte35 <81330498+JhoanDuarte35@users.noreply.github.com>
2023-02-17 16:51:34 -0500

    Add files via upload

---
+++ chat-master/crearGrupos.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+include_once "php/config.php";
+if (!isset($_SESSION['unique_id'])) {
+  header("location: login.php");
+}
+?>
+<?php include_once "header.php"; ?>
+
+<body>
+  <div class="wrapper">
+    <section class="users">
+      <header>
+        <div class="content">
+          <?php
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+          if (mysqli_num_rows($sql) > 0) {
+            $row = mysqli_fetch_assoc($sql);
+          }
+          ?>
+          <img src="php/images/<?php echo $row['img']; ?>" alt="">
+          <div class="details">
+            <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+            <p><?php echo $row['status']; ?></p>
+          </div>
+          <a href="users.php" class="logout">Volver</a>
+        </div>
+        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Cerrar Sesión</a>
+      </header>
+        <div>Miembros del Grupo</div>
+      <div class="users-list-group">
+        
+
+      </div>
+  
+      <div class="search">
+        <span class="text">Agrega Usuarios a tu grupo</span>
+        <input type="text" placeholder="Busca usuarios">
+        <button><i class="fas fa-search"></i></button>
+      </div>
+      <div class="users-list">
+
+      </div>
+      <form action="" type="POST">
+      <input type="text" placeholder="Nombre del grupo">
+      <input type="submit" value="Crear Grupo">
+      </form>
+    </section>
+  </div>
+
+  <script src="javascript/crearGrupos.js"></script>
+
+</body>
+
+</html>
---
+++ chat-master/javascript/crearGrupos.js
@@ -0,0 +1,81 @@
+const searchBar = document.querySelector(".search input"),
+searchIcon = document.querySelector(".search button"),
+usersList = document.querySelector(".users-list"),
+usersListGroup = document.querySelector(".users-list-group"),
+buttonadd=document.querySelector(".btn btn-outline-success");
+
+searchIcon.onclick = ()=>{
+  searchBar.classList.toggle("show");
+  searchIcon.classList.toggle("active");
+  searchBar.focus();
+  if(searchBar.classList.contains("active")){
+    searchBar.value = "";
+    searchBar.classList.remove("active");
+  }
+}
+
+searchBar.onkeyup = ()=>{
+  let searchTerm = searchBar.value;
+  if(searchTerm != ""){
+    searchBar.classList.add("active");
+  }else{
+    searchBar.classList.remove("active");
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/busquedauser.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          usersList.innerHTML = data;
+        }
+    }
+  }
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("searchTerm=" + searchTerm);
+}
+
+setInterval(() =>{
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "php/users-grup.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          if(!searchBar.classList.contains("active")){
+            usersList.innerHTML = data;
+          }
+        }
+    }
+  }
+  xhr.send();
+}, 500);
+
+    var array=[];
+
+function myfuncion(id){
+    alert(id);
+    let pos = array.indexOf(id);
+    if(array[pos]==id){
+        console.log(array); 
+    }else{
+        array.push(id);
+        console.log(array); 
+        document.getElementById(id).disabled = true;
+    }
+
+    let id_user_group = id;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/busquedauser.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+            let data = xhr.response;
+            usersListGroup.innerHTML = data;
+          }
+      }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("id_user_group=" + id_user_group);
+}
+
